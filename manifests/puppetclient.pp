7a1ae521e1bd3c4b1d04f1adf8c92ea0f672f9e0
root <root@htmkvh06.muctst2.elster.de>
2016-07-12 15:50:39 +0200

    added vagrant boxes

--- manifests/puppetclient.pp
+++ manifests/puppetclient.pp
@@ -1,3 +1,3 @@
-node puppetclient1204.vagrant.local, puppetclient1404.vagrant.local, puppetclient1604.vagrant.local, {
+node puppetclient1204.vagrant.local, puppetclient1404.vagrant.local, puppetclient1604.vagrant.local {
 include role::puppetclient
 }
