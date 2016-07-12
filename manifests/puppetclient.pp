c64450bb9724bc172006ca972da3ebe6bbed4faf
root <root@htmkvh06.muctst2.elster.de>
2016-07-12 15:44:46 +0200

    added vagrant boxes

---
+++ manifests/puppetclient.pp
@@ -0,0 +1,3 @@
+node puppetclient1204.vagrant.local, puppetclient1404.vagrant.local, puppetclient1604.vagrant.local, {
+include role::puppetclient
+}
---
+++ site/profile/manifests/puppetclient.pp
@@ -0,0 +1,4 @@
+class profile::puppetclient {
+include profile::base
+#spaceholder
+}
